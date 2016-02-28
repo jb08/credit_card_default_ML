26742ca6ea9a86b10f906e4de70b32a0184026b1
Jason Brown <jasonkingsley.brown@gmail.com>
2016-02-28 17:21:40 -0600

    data into pandas

--- credit_card_defaults.py
+++ credit_card_defaults.py
@@ -3,8 +3,15 @@ import sys
 from pandas import DataFrame
 
 
+def csv_reader(file_name):
+	
+	frame = pd.read_csv(file_name, index_col = 0, header = 0, skiprows = [1])
+	return frame
+
 def main():
-	print "main()"
+	path = "default of credit card clients.csv";
+	frame = csv_reader(path)
+	print frame.head(10)
 
 if __name__ == "__main__":
 	main()
